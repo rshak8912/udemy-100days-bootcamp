287fb381e8e3c4a39385127edd339767a97733ae
rshak8912 <rshak8912@gmail.com>
2021-01-02 21:44:57 +0900

    day1 code added

--- day1/day1.py
+++ day1/day1.py
@@ -1,16 +1,27 @@
 #Write your code below this line 👇
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
-
-print("Hello world!\nHello world!\n")
-print("Hello" + "Angela")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+#
+# print("Hello world!\nHello world!\n")
+# print("Hello" + "Angela")
 
 #Fix the code below 👇
 
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
-print("e.g. print(\"Hello  \"+ \"world\")")
-print("New lines can be created with a backslash and n.")
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+# print("e.g. print(\"Hello  \"+ \"world\")")
+# print("New lines can be created with a backslash and n.")
 
-print("Hello " + input("What is your name? "))
+#print("Hello " + input("What is your name? "))
+##############################################################
+print("Welcome to the band name generator.")
+
+city = input("Which city did you grow up in?")
+
+pet = input("What is the name of a pet?\n")
+
+print("Your band name could be " + city + " " + pet)
+
+
+
