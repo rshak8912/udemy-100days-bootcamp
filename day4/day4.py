a08c59b9522bd3776a368cb752ad5a1e8d458830
rshak8912 <rshak8912@gmail.com>
2021-01-05 22:35:47 +0900

    Add day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,6 +1,28 @@
 import random
 import my_module
 
-random_integer = random.randint(1, 10)
-print(random_integer)
-print(my_module.pi)
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+# print(my_module.pi)
+#
+# random_float = random.random()
+# print(str(random_float * 5)[0])
+
+# random_side = random.randint(0, 1)
+# print(random_side)
+# if random_side == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+# states_of_america=["Dela", "Redmond", "Hawaii"]
+# states_of_america.append("virtualland")
+# print(states_of_america[-1])
+
+names_string = input("name list>> ")
+names = names_string.split(", ")
+print(names)
+
+num_items = len(names)
+random_choice = random.randint(0, num_items - 1)
+print(names[random_choice])
