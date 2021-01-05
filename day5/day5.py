fbf82dad53da555611f9e5dd739eafd596f5dce6
rshak8912 <rshak8912@gmail.com>
2021-01-05 23:42:40 +0900

    Add day5

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,19 @@
-fruits = ["A", "B", "C"]
+# fruits = ["A", "B", "C"]
+#
+# for fruit in fruits:
+#     print(fruit)
+#
+# for i in range(0, 5):
+#     print(i)
 
-for fruit in fruits:
-    print(fruit)
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
 
+total2 = 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        total2 += number
+
+print(total2)
