ab29fb4322bfa9e32b86a5d588d0f0398af83e69
rshak8912 <rshak8912@gmail.com>
2021-01-06 15:28:14 +0900

    Add day6 - loop

---
+++ day6/day6.py
@@ -0,0 +1,17 @@
+def my_functtion():
+  print("2space")
+
+def my_function2():
+    print("1tab, 4space")
+
+my_functtion()
+my_function2()
+
+def my_function3():
+  print("2space")
+  print("2space")
+
+my_function3()
+
+
+
