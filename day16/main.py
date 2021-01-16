8278a06fb937c7551e35fbaa00f36f9b7db45288
rshak8912 <rshak8912@gmail.com>
2021-01-17 02:55:48 +0900

    Add day16 - Object Oriented Programming (OOP)

--- day16/main.py
+++ day16/main.py
@@ -1,15 +1,26 @@
-# import another_module
-# print(another_module.another_variable)
+# # import another_module
+# # print(another_module.another_variable)
+#
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
 
+from prettytable import PrettyTable
 
+table = PrettyTable()
+table.add_column("Poketmon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table.align = "l"
+
+print(table)
