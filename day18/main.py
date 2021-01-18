4f3058817e80b347b5e062a52a0138183b5e9b3f
rshak8912 <rshak8912@gmail.com>
2021-01-19 01:30:10 +0900

    Add day18 - Turtle & the Graphical User Interface (GUI)

---
+++ day18/main.py
@@ -0,0 +1,34 @@
+# from turtle import Turtle, Screen
+
+#tim = Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("red")
+# timmy_the_turtle.forward(100)
+#
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+#
+# screen = Screen()
+# screen.exitonclick()
+
+# from random import *
+#
+# a = choice([1,2,3])
+# print(a)
+
+# import turtle as t
+#
+# tim = t.Turtle()
+
+# import heroes
+#
+# for _ in range(5):
+#     print(heroes.gen())
+
+
