b2610084036304477e8a57dd10601b180694267e
rshak8912 <rshak8912@gmail.com>
2021-01-18 23:06:49 +0900

    Add day17 - The Quiz Project & the Benefits of OOP

---
+++ day17/main.py
@@ -0,0 +1,15 @@
+class User:
+    pass
+
+user_1 = User()
+user_1.id = "001"
+user_1.username = "angela"
+
+print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.name = "jack"
+
+print(user_2.name)
+
