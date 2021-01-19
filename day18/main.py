058685c036fa486fd71f99be2aff73c616a6cffa
rshak8912 <rshak8912@gmail.com>
2021-01-19 15:44:39 +0900

    Add day18 - Turtle & the Graphical User Interface (GUI)

--- day18/main.py
+++ day18/main.py
@@ -30,5 +30,16 @@
 #
 # for _ in range(5):
 #     print(heroes.gen())
-
+#
+# import turtle as t
+#
+# tim = t.Turtle()
+#
+# def draw_shape(num_sides):
+#     for _ in range(num_sides):
+#         angle = 360 / num_sides
+#         tim.forward(100)
+#         tim.right(angle)
+# for shape_side_n in range(3, 11):
+#     draw_shape(shape_side_n)
 
