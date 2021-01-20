f96984b7851f413ca86c6dd6fa4385cbfc8f5975
rshak8912 <rshak8912@gmail.com>
2021-01-21 00:13:08 +0900

    Add day20 - Build the Snake Game Part 1: Animation & Coordinates

--- day20/main.py
+++ day20/main.py
@@ -1,8 +1,33 @@
 from turtle import Screen, Turtle
-
+from snake import Snake
+import time
 screen = Screen()
-screen.setup(width=680, height = 680)
+screen.setup(width=600, height = 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-segment_1 = Turtle()
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
+segments = []
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+
+screen.exitonclick()
